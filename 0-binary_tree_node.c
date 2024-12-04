164fcf3399acbdfaef4c5986de1ab39b74a0b90e
nepodude <nepo.jean73@gmail.com>
2024-12-04 17:37:45 +0200

    added something to my README

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,19 +1,26 @@
 #include "binary_trees.h"
 
+/**
+* binary_tree_node - function for creating just a leaf node.
+* @parent: parent to our node.
+* @value: value inside our node.
+* Return: Returns the pointer to created node.
+*/
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *my_tree;
+	binary_tree_t *my_tree;
 
-    my_tree = malloc(sizeof(binary_tree_t));
-    if (my_tree == NULL)
-    {
-        return (NULL);
-    }
+	my_tree = malloc(sizeof(binary_tree_t));
+	if (my_tree == NULL)
+	{
+		return (NULL);
+	}
 
-    my_tree->parent = parent;
-    my_tree->value = value;
-    my_tree->left = NULL;
-    my_tree->right = NULL;
+	my_tree->parent = parent;
+	my_tree->value = value;
+	my_tree->left = NULL;
+	my_tree->right = NULL;
 
-    return (my_tree);
+	return (my_tree);
 }
