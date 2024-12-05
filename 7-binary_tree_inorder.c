582b529a746a585bfdec4433bd9cd61fbccb7938
nepodude <nepo.jean73@gmail.com>
2024-12-05 11:11:57 +0200

    task5 updated

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_inorder - treverses the tree using inorder way.
+* @tree: the root to start with.
+* @func: the function to cast the node's value into.
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    
+}
