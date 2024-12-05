da3f7b761d4b4d558c72922e4e6b6d8783d0ece7
nepodude <nepo.jean73@gmail.com>
2024-12-05 10:22:28 +0200

    task3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - deletes a binary tree.
+* @tree: a pointer to the tree to delete
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	if (tree->left != NULL)
+	{
+		binary_tree_delete(tree->left);
+	}
+	if (tree->right != NULL)
+	{
+		binary_tree_delete(tree->right);
+	}
+	free(tree);
+}
