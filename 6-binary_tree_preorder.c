69f400ec82854366091a8e259ff3b7a287873aed
nepodude <nepo.jean73@gmail.com>
2024-12-05 11:16:11 +0200

    task6 and 7 updated

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,7 +4,6 @@
 * binary_tree_preorder - traverses a list in a preorder style.
 * @tree: root node of the tree to traverse.
 * @func: pointer to the function to call each node.
-* 
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
