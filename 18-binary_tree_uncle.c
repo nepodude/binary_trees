87d5738395dbf28181382edbb8bb64d0a9f9f86e
nepodude <nepo.jean73@gmail.com>
2024-12-05 17:08:20 +0200

    updated task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent->parent || node->parent == NULL)
+	if (node == NULL || node->parent->parent == NULL || node->parent == NULL)
 		return (NULL);
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
