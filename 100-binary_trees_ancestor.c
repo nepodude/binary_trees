74643d548f7a07eb08e573a119598322588ce8bb
nepodude <nepo.jean73@gmail.com>
2024-12-05 17:36:53 +0200

    updated task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,9 @@
 * Return: pointer to the lowest common ancestor, or NULL if no common ancestor.
 */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(
+	const binary_tree_t *first, const binary_tree_t *second
+)
 {
 	const binary_tree_t *first_ancestor = first;
 	const binary_tree_t *second_ancestor = second;
