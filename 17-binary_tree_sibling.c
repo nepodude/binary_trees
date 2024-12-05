67114eba0693ceddfd235bdafc5db6d5a289c6b7
nepodude <nepo.jean73@gmail.com>
2024-12-05 16:55:35 +0200

    updated task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-
 /**
  * binary_tree_sibling - returns sibling of a node.
  * @node: our node.
@@ -9,12 +8,11 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (parent->left == node)
-	{
-		return (&(parent->right));
-	}
-	if (parent->right == node)
-	{
-		return (&(parent->left));
-	}
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	if (node->parent->right == node)
+		return (node->parent->left);
+	return (NULL);
 }
