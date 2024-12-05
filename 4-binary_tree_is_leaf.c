347dd73a65d79823d7db5e6c077221318a6d0cca
nepodude <nepo.jean73@gmail.com>
2024-12-05 10:32:11 +0200

    task4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,7 +8,11 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL || (node->right == NULL && node->left == NULL))
+	if (node == NULL)
+	{
+		return (1);
+	}
+	if (node->right == NULL && node->left == NULL)
 	{
 		return (1);
 	}
