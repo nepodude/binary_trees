9aa77467a3c6e5106be3d048410aafcf445f2a87
nepodude <nepo.jean73@gmail.com>
2024-12-05 15:36:54 +0200

    updated task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,15 +8,18 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int result_left, result_right;
+	size_t result_left, result_right;
+
+    result_left = 0;
+    result_right = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	result_left = (int *)binary_tree_height(tree->left);
-	result_right = (int *)binary_tree_height(tree->right);
+	result_left = binary_tree_height((const binary_tree_t *)(tree->left));
+	result_right = binary_tree_height((const binary_tree_t *)(tree->right));
 
-	return (result_left - result_right);
+	return ((int)(result_left - result_right));
 }
 
 /**
@@ -24,7 +27,7 @@ int binary_tree_balance(const binary_tree_t *tree)
  * @tree: Pointer to the root node of the tree to measure the height.
  * Return: Height of the tree, or 0 if tree is NULL.
  */
-size_t binary_tree_height(binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0;
 	size_t right_height = 0;
@@ -34,25 +37,13 @@ size_t binary_tree_height(binary_tree_t *tree)
 		return (0);
 	}
 
-	/* Recursively calculate the height of the left subtree */
 	if (tree->left != NULL)
-	{
 		left_height = binary_tree_height(tree->left) + 1;
-	}
-
-	/* Recursively calculate the height of the right subtree */
 	if (tree->right != NULL)
-	{
 		right_height = binary_tree_height(tree->right) + 1;
-	}
 
-	/* Compare and return the greater height */
 	if (left_height > right_height)
-	{
 		return (left_height);
-	}
 	else
-	{
 		return (right_height);
-	}
 }
