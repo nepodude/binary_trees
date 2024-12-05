d508e471a8e6661c0e5f0a9a3cdf5e58a488e721
nepodude <nepo.jean73@gmail.com>
2024-12-05 13:22:44 +0200

    updaged task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_size - counts the number of nodes there is.
+* @tree: root to the tree we have.
+* Return: returns the number of nodes there is.
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t my_size = 1;
+
+	if (tree == NULL)
+		return (0);
+	
+	my_size += binary_tree_size(tree->left);
+	my_size += binary_tree_size(tree->right);
+	return (my_size);
+}
